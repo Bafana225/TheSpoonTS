6a18d8cb6eaaf55ac4c3553dfd2b819927706106
verybadgag <memel_esmel@hotmail.fr>
2023-04-05 15:41:39 +0200

    INIT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'add-restaurant', component: AddRestaurantComponent },
+{ path: 'reservations', component: ReservationsComponent },
+{ path: 'restaurants', component: RestaurantsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
